f0757d569b3e8a95486c6e4704493de7b8b19c71
— <drcmda@gmail.com>
2022-06-20 14:43:40 +0200

    remove pluginmanager, memoize effects chain

--- src/components/canvas/Effects.tsx
+++ src/components/canvas/Effects.tsx
@@ -20,26 +20,6 @@ export function Composer({
 }: any) {
   const hovered = useDrawing(drawingId, d => d.interaction.hovered)
   const selected = useDrawing(drawingId, d => d.interaction.selected)
-
-  const hoveredMeshes = useOutlinesStore(s => s.outlinedMeshes['hovered'])
-  const selectedMeshes = useOutlinesStore(s => s.outlinedMeshes['selected'])
-
-  const selections1 = React.useMemo(() => selectedMeshes? Object.values(selectedMeshes) : [], [selectedMeshes])
-  const selections2 = React.useMemo(() => hoveredMeshes? Object.values(hoveredMeshes) : [], [hoveredMeshes])
-
-  /* const merged = React.useMemo(() => {
-    const selectedArr = Object.values(selectedMeshes || {})
-
-    if (selectedArr.length > 3) {
-      const merged_: THREE.Object3D[] = []
-      selectedArr.forEach(selected => merged_.push(...selected))
-
-      return merged_
-    }
-
-    return []
-  }, [selectedMeshes]) */
-
   // Skip outlines when selection is active
   // const selectionActive = useBuerli(s => !!s.drawing.refs[s.drawing.active!]?.selection.active)
   // Skip AO when sketch is active
@@ -48,23 +28,18 @@ export function Composer({
     const plugin = drawing ? drawing.plugin.refs[drawing.plugin.active.feature!] : null
     return plugin ? drawing.structure.tree[plugin.id]?.class === CCClasses.CCSketch : false
   })
-
   // Decide if effects-chain is active or not
   const enabled = !sketchActive
-
   return (
     <>
-      <EffectComposer enabled={enabled} multisampling={8} autoClear={false} {...props}>
-        <SSAO radius={radius} intensity={85} luminanceInfluence={0.2} color="black" />
-        <Outline
-          selections1={selections1}
-          selections2={selections2}
-          selectionLayer={10}
-          edgeColor1={selectedColor as any}
-          edgeColor2={hoveredColor as any}
-          edgeStrength={edgeStrength}
-        />
-      </EffectComposer>
+      <Chain
+        enabled={enabled}
+        hoveredColor={hoveredColor}
+        selectedColor={selectedColor}
+        edgeStrength={edgeStrength}
+        radius={radius}
+        {...props}
+      />
       {hovered && <OutlinedObjects drawingId={drawingId} info={hovered} group="hovered" />}
       {selected?.map(info => (
         <OutlinedObjects key={info.objectId} drawingId={drawingId} info={info} group="selected" />
@@ -74,3 +49,38 @@ export function Composer({
     </>
   )
 }
+
+// Make the effects chain a stable, memoized component
+const Chain = React.memo(({ enabled, radius, hoveredColor, selectedColor, edgeStrength, ...props }: any) => {
+  return (
+    <EffectComposer enabled={enabled} multisampling={8} autoClear={false} {...props}>
+      <SSAO radius={radius} intensity={85} luminanceInfluence={0.2} color="black" />
+      <MultiOutline
+        hoveredColor={hoveredColor}
+        selectedColor={selectedColor}
+        edgeStrength={edgeStrength}
+        radius={radius}
+      />
+    </EffectComposer>
+  )
+})
+
+// The outline component will update itself without disturbing the parental effect composer
+const MultiOutline = React.memo(
+  ({ hoveredColor = 'white', selectedColor = 'white', edgeStrength = 100, radius = 0.1 }: any) => {
+    const hoveredMeshes = useOutlinesStore(s => s.outlinedMeshes['hovered'])
+    const selectedMeshes = useOutlinesStore(s => s.outlinedMeshes['selected'])
+    const selections1 = React.useMemo(() => (selectedMeshes ? Object.values(selectedMeshes) : []), [selectedMeshes])
+    const selections2 = React.useMemo(() => (hoveredMeshes ? Object.values(hoveredMeshes) : []), [hoveredMeshes])
+    return (
+      <Outline
+        selections1={selections1}
+        selections2={selections2}
+        selectionLayer={10}
+        edgeColor1={selectedColor as any}
+        edgeColor2={hoveredColor as any}
+        edgeStrength={edgeStrength}
+      />
+    )
+  },
+)
