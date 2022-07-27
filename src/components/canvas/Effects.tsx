441c91968e11a76f51574c391de89b353a99fa36
Daniel Manser <daniel.manser@awv-informatik.ch>
2022-07-27 10:04:09 +0200

    Add option to be able to disable ssao

--- src/components/canvas/Effects.tsx
+++ src/components/canvas/Effects.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/display-name */
 import React from 'react'
 
 import { EffectComposer, SSAO } from '@react-three/postprocessing'
@@ -16,6 +17,7 @@ export function Composer({
   selectedColor = 'white',
   edgeStrength = 100,
   radius = 0.1,
+  ssao = true,
   ...props
 }: any) {
   const hovered = useDrawing(drawingId, d => d.interaction.hovered)
@@ -27,7 +29,7 @@ export function Composer({
     const drawing = s.drawing.refs[s.drawing.active!]
     const plugin = drawing ? drawing.plugin.refs[drawing.plugin.active.feature!] : null
     const objClass = drawing.structure.tree[plugin?.id || -1]?.class || ''
-    return ccUtils.base.isA(objClass, CCClasses.CCSketch) 
+    return ccUtils.base.isA(objClass, CCClasses.CCSketch)
   })
   // Decide if effects-chain is active or not
   const enabled = !sketchActive
@@ -39,6 +41,7 @@ export function Composer({
         selectedColor={selectedColor}
         edgeStrength={edgeStrength}
         radius={radius}
+        ssao={ssao}
         {...props}
       />
       {hovered && <OutlinedObjects drawingId={drawingId} info={hovered} group="hovered" />}
@@ -52,19 +55,21 @@ export function Composer({
 }
 
 // Make the effects chain a stable, memoized component
-const Chain = React.memo(({ enabled, radius, hoveredColor, selectedColor, edgeStrength, ...props }: any) => {
-  return (
-    <EffectComposer enabled={enabled} multisampling={8} autoClear={false} {...props}>
-      <SSAO radius={radius} intensity={85} luminanceInfluence={0.2} color="black" />
-      <MultiOutline
-        hoveredColor={hoveredColor}
-        selectedColor={selectedColor}
-        edgeStrength={edgeStrength}
-        radius={radius}
-      />
-    </EffectComposer>
-  )
-})
+const Chain = React.memo(
+  ({ enabled, radius, hoveredColor, selectedColor, edgeStrength, ssao = true, ...props }: any) => {
+    return (
+      <EffectComposer enabled={enabled} multisampling={8} autoClear={false} {...props}>
+        {ssao && <SSAO radius={radius} intensity={85} luminanceInfluence={0.2} color="black" />}
+        <MultiOutline
+          hoveredColor={hoveredColor}
+          selectedColor={selectedColor}
+          edgeStrength={edgeStrength}
+          radius={radius}
+        />
+      </EffectComposer>
+    )
+  },
+)
 
 // The outline component will update itself without disturbing the parental effect composer
 const MultiOutline = React.memo(
