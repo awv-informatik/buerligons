1214c2108385ac0d9aa7a7697aa51647ba91a06c
STINGNAILS <stingnails@ledas.com>
2024-06-12 00:22:31 +0700

    Added GlobalCSysDisplay

---
+++ src/components/canvas/GlobalCSysDisplay.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import { DrawingID } from '@buerli.io/core'
+import { useDrawing } from '@buerli.io/react'
+import { CSysDisplay, CSysDisplayMode } from '@buerli.io/react-cad'
+
+const pluginBlacklist = ['Fastened Origin', 'Fastened', 'Slider', 'Revolute', 'Cylindrical', 'Planar', 'Parallel']
+
+export const GlobalCSysDisplay: React.FC<{ drawingId: DrawingID }> = ({ drawingId }) => {
+  const activePluginId = useDrawing(drawingId, drawing => drawing.plugin.active.feature)
+  const activePluginName = useDrawing(drawingId, drawing => drawing.plugin.refs[activePluginId || -1]?.name)
+
+  const isDisplayed = !activePluginName || !pluginBlacklist.some(plugin => activePluginName === plugin)
+
+  return isDisplayed ? <CSysDisplay drawingId={drawingId} displayMode={CSysDisplayMode.DisplayVisible} /> : null
+}
--- src/components/canvas/index.ts
+++ src/components/canvas/index.ts
@@ -2,9 +2,10 @@ import { AutoClear } from './AutoClear'
 import { Controls } from './Controls'
 import { Composer } from './Effects'
 import { Fit } from './Fit'
+import { GlobalCSysDisplay } from './GlobalCSysDisplay'
 import { Lights } from './Lights'
 import { raycastFilter } from './RaycastFilter'
 import { Threshold } from './Threshold'
 import { GeometryInteraction, HighlightedObjects, CanvasContextMenu, useContextMenuItems } from './Interaction'
 
-export { AutoClear, Fit, Lights, Controls, Composer, raycastFilter, Threshold, GeometryInteraction, HighlightedObjects, CanvasContextMenu, useContextMenuItems }
+export { AutoClear, Fit, Lights, Controls, Composer, raycastFilter, Threshold, GeometryInteraction, HighlightedObjects, CanvasContextMenu, useContextMenuItems, GlobalCSysDisplay }
