39f4df0cba39a9cedf3da6eed61a4ff184ac15f8
— <drcmda@gmail.com>
2025-04-23 13:39:57 +0200

    remove docusaurus temp folder

--- .docusaurus/@easyops-cn/docusaurus-search-local/default/generated-constants.js
+++
@@ -1,6 +0,0 @@
-import lunr from "/Users/ph/dev/buerli/buerligons.io/node_modules/lunr/lunr.js";
-export const removeDefaultStopWordFilter = false;
-export const language = ["en"];
-export const searchIndexUrl = "search-index{dir}.json?_=4a339be8";
-export const searchResultLimits = 8;
-export const fuzzyMatchingDistance = 1;
--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/ph/dev/buerli/buerligons.io/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/ph/dev/buerli/buerligons.io/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/ph/dev/buerli/buerligons.io/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/ph/dev/buerli/buerligons.io/src/css/custom.css"),
-];
