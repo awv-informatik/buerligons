ae268018a3daa72cfa1652666754395195167624
dm <daniel.manser@awv-informatik.ch>
2025-12-19 10:42:44 +0100

    Delete everything about electron (#80)

--- src/components/Startup.tsx
+++ src/components/Startup.tsx
@@ -1,24 +1,15 @@
 import { useBuerli } from '@buerli.io/react'
 import React from 'react'
-import { useIPC } from '../ipc'
-import { ChooseCCApp } from './ChooseCCApp'
-import { WelcomePage } from './WelcomePage'
 import { Buerligons } from './Buerligons'
+import { WelcomePage } from './WelcomePage'
 
 export const Startup: React.FC = () => {
   const count = useBuerli(s => s.drawing.ids.length)
   const drawingId = useBuerli(s => s.drawing.active || '')
-  const ipc = useIPC()
   React.useEffect(() => void (document.title = 'buerligons'), [])
   return (
     <div style={{ backgroundColor: '#fff', height: '100%', width: '100%' }}>
-      {ipc.isEmbeddedApp && !ipc.hasClassFile ? (
-        <ChooseCCApp />
-      ) : count === 0 || !drawingId ? (
-        <WelcomePage />
-      ) : (
-        <Buerligons />
-      )}
+      {count === 0 || !drawingId ? <WelcomePage /> : <Buerligons />}
     </div>
   )
 }
